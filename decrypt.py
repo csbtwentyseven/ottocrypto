77c8d05a1422fa911b656c50ee272e9902833d39
BERKGILLER <csaidberk@gmail.com>
2020-05-22 17:52:42 +0300

    OE MODUNDA DECRYPTION SORUNSUZ CALISIYOR OA MODUNA BASLANDI!

--- decrypt.py
+++ decrypt.py
@@ -13,17 +13,17 @@ class Kir():
 
 
     def oeSade(hash): # oe modunu sadeleştirir.
-        dosya = open('parcasade.txt', 'w')
+        dosyaoe = open('parcasade.txt', 'w')
 
         parcailk = hash[16:len(hash)-17]
         parcasade = parcailk[:int(len(parcailk) / 2)] #hash'ı fazlalıklardan arındırıyoruz
 
-        dosya.write(parcasade)
-        dosya.close()
+        dosyaoe.write(parcasade)
+        dosyaoe.close()
         Kir.decode()
 
-    def oAsade(self): #oa modunu sadeleştirir.
-        pass
+    def oaSade(self): #oa modunu sadeleştirir.
+        print("Sa")
 
 
     def decode(): #gelen sade hash'in sayisal verilerini ayıklar.
@@ -51,15 +51,51 @@ class Kir():
         Eslestir.sayidanharf()
 
 class Eslestir():
+    osman_tablo = {"21": "a",
+                   "Q2": "b",
+                   "Q6": "c",
+                   "Q7": "ç",
+                   "10": "d",
+                   "Q1": "e",
+                   "23": "f",
+                   "26": "g",
+                   "22": "ğ",
+                   "Q8": "h",
+                   "37": "ı",
+                   "38": "i",
+                   "14": "j",
+                   "24": "k",
+                   "28": "l",
+                   "29": "m",
+                   "27": "n",
+                   "39": "o",
+                   "40": "ö",
+                   "Q3": "p",
+                   "12": "r",
+                   "Q5": "s",
+                   "16": "ş",
+                   "Q4": "t",
+                   "41": "u",
+                   "42": "ü",
+                   "31": "v",
+                   "36": "y",
+                   "11": "z",
+                   "T": "T",
+                   "C": "C",
+                   "1453": " ",
+                   }  # latin - osmanlı alfabe karşılığı
+
     def sayidanharf():
         sayisalTM = open('decrypt.txt', 'r').read() # sayisalTM = Sayısal Temiz Metin
         j = 0 #döngü index degiskeni
+        sozelTM = list() #sozelTM = Sözel Temiz Metin
         for i in sayisalTM:
             if(j%2 == 0):
-                print(sayisalTM[j] + sayisalTM[j+1])
-            j = j+1
+                sozelTM.append(Eslestir.osman_tablo[sayisalTM[j] + sayisalTM[j+1]]) #sayilari harflerle eslestirip sozelTM listesine sırasıyla ekliyoruz
+            j = j+1 # index arttır.
 
-
+            temiz_metin = ''.join(map(str, sozelTM)) #sozelTM listemizi stringe ceviriyoruz.
+        print(temiz_metin)
 
 
 
@@ -70,7 +106,7 @@ def main():
         Kir.oeSade(csb)
 
     elif(Kir.modCek(csb) == "oa"):
-        Kir.oASade(csb)
+        Kir.oaSade(csb)
 
 if __name__ == '__main__':
     main()
