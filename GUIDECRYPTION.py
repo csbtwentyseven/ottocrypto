5f299cf24455ef7a549647065d7c8cb1ae410b4a
csb27 <38182497+csbtwentyseven@users.noreply.github.com>
2020-05-25 11:30:06 -0400

    file not found error fix

--- GUIDECRYPTION.py
+++ GUIDECRYPTION.py
@@ -129,7 +129,7 @@ class Eslestir():
 
             temiz_metin = ''.join(map(str, sozelTM)) #sozelTM listemizi stringe ceviriyoruz.
      
-        dosyaYaz = open('duz_metin.txt', 'w')
+        dosyaYaz = open("duz_metin.txt", 'w')
         dosyaYaz.write(temiz_metin)
 
 class Ui_OttoCrypto(object):
@@ -199,16 +199,17 @@ class Ui_OttoCrypto(object):
 
 
     def main(self):
-        dosyaOku = open('duz_metin.txt', 'r').read()
         global csb
         csb = self.input.text().strip()
         if(time.strftime("%H"+"%M") == "0415"): #saat 4.15 ise kırılım işlemini başlat
+        if(True):
             if(Kir.modCek(csb) == "oe"): # mod oe ise
                 Kir.oeSade(csb) #oe sadeleştirmeyi başlat
 
             elif(Kir.modCek(csb) == "oa"):#mod oa ise
                 Kir.oaSade(csb)#oa sadeleştirmeyi başlat.
-            
+
+            dosyaOku = open('duz_metin.txt', 'r').read()
             self.textBrowser.setText(dosyaOku)
         else:
             self.textBrowser.setText("56.38179557644142Q2C4721C9728T7937C4324T97Q7T4737T671453T73Q6T2338T23Q8C4721T5927T1126C4338C6112T6776T2116C8334C6211T7295T1274C8Q32T8332T6Q37T354176T7374T7Q79T4234C7397T8279C1274C2Q29.003378456544905")
